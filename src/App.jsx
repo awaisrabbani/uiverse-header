3819ec9df1664ae7c70b6a5379a51bcf91527c48
AwaisRabbani0987654 <awaisrabbani777@gmail.com>
2024-10-21 22:49:14 +0500

    Tailwind + vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-
-
 function App() {
-
-
   return (
     <>
-   <h1> Hello</h1>
+    <div className="container bg-black ">
+      <h1 className="text-3xl font-bold underline text-white">Hello world!</h1>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
